bd00f522e00564a25c5f54b5070570dc052bb49e
staceyjf <staceyfanner@gmail.com>
2024-04-06 13:03:25 +1100

    basic branding adds

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,11 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <div className="flexbox">
       <Navbar />
       <Home />
       <Results />
-    </>
+    </div>
   );
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,13 @@
 import styles from "./Navbar.module.scss";
 
 function Navbar() {
-  return <nav className={styles.nav}>This is a nav</nav>;
+  return (
+    <nav className={styles.nav}>
+      <div className={styles.logo_container}>LOGO</div>
+      <div className={styles.searchbar_container}>SEARCHBAR</div>
+      <div className={styles.icons_container}>ICONS</div>
+    </nav>
+  );
 }
 
 export default Navbar;
