2892088f918ebd6376e4057d0f7863d112774208
staceyjf <staceyfanner@gmail.com>
2024-04-07 15:03:29 +1000

    basic homepage splash add

--- src/containers/HomeContainer/HomeContainer.jsx
+++ src/containers/HomeContainer/HomeContainer.jsx
@@ -1,9 +1,15 @@
 import styles from "./HomeContainer.module.scss";
+import PeopleIllustration from "../../components/PeopleIllustration/PeopleIllustration";
 
 function HomeContainer() {
   return (
     <section className={styles.home}>
-      This is where the splash page goes
+      <div className={styles.container}>
+        <h2>Welcome to the bookVerse</h2>
+        <div className={styles.img_wrapper}>
+          <PeopleIllustration />
+        </div>
+      </div>
     </section>
   );
 }
