e421caef138197fb6792034e439e354dd6b73a26
staceyjf <staceyfanner@gmail.com>
2024-04-09 08:58:15 +1000

    tweak to yml to inlcude GH secert

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default ({ mode }) => {
+export default ({ command, mode }) => {
   const env = loadEnv(mode, process.cwd());
 
   // Convert .env variables so i can use process.env.nameoenv
